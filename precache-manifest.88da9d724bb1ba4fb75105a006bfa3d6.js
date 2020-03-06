48c210638b6cf6fa03f685b7f7767d4ee2f0511a
munaalshorman <runner@fv-az78.2j3110p3mdjutbomsffdnuoxxg.cx.internal.cloudapp.net>
2020-03-06 19:47:20 +0000

    Updates

---
+++ precache-manifest.88da9d724bb1ba4fb75105a006bfa3d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1cc943a100fce25c672a8ff7ac5111",
+    "url": "/lab-28-props-and-state/index.html"
+  },
+  {
+    "revision": "7a4a39600564d59c2c88",
+    "url": "/lab-28-props-and-state/static/css/main.52f66228.chunk.css"
+  },
+  {
+    "revision": "5095119f9d2cf7cd317f",
+    "url": "/lab-28-props-and-state/static/js/2.534ed864.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/lab-28-props-and-state/static/js/2.534ed864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a4a39600564d59c2c88",
+    "url": "/lab-28-props-and-state/static/js/main.aacde463.chunk.js"
+  },
+  {
+    "revision": "f0bb162b17a50191c24a",
+    "url": "/lab-28-props-and-state/static/js/runtime-main.06bb42c1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/401-advanced-javascript-muna/lab-28-props-and-state/precache-manifest.b4494620eb5d1e2fefabb22756a78509.js"
+  "/lab-28-props-and-state/precache-manifest.88da9d724bb1ba4fb75105a006bfa3d6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/401-advanced-javascript-muna/lab-28-props-and-state/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lab-28-props-and-state/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
